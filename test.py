d12804f5fbefa2ca83ef119f3a144f60ad5bc932
marioncharpentier <charpentier.marion.c@gmail.com>
2017-10-07 14:42:51 +0200

    My Django Girls app, first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+print('hello')
+if 3<2:
+    print ('it\'s working')
+else:
+    print ('hey')
+def marion(name):
+    if name=='marion':
+        print('salutmarion')
+    else:
+        print('salutpasmarion')
+marion('marion')
+marion('francesca')
+girls=['marion1','marion2','charlotte','francesca']
+for name in girls:
+    print('hello'+name+'!')
